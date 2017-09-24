44237fd443c560f80c6389002f80d5719998f16d
Konsta Toivanen <31590408+konstatoivanen@users.noreply.github.com>
2017-09-24 23:31:24 +0300

    Update

---
+++ DistortGUI.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.ImageEffects;
+
+public class DistortGUI : MonoBehaviour
+{
+    public Slider slider_downscale;
+    public Slider slider_zoom;
+    public Slider slider_fractalCoverage;
+    public Slider slider_painting;
+    public Slider slider_entropy;
+    public Slider slider_entropyOscA;
+    public Slider slider_entropyOscS;
+    public Slider slider_fisheye;
+    public Slider slider_characterPosition;
+    public Transform character;
+    public DistortImageEffect distort;
+
+
+	void Update ()
+    {
+        distort.downScale               =  slider_downscale.value;
+        distort.zoom                    = -slider_zoom.value;
+        distort.fractalCoverage         =  slider_fractalCoverage.value;
+        distort.painting                =  slider_painting.value; 
+        distort.entropy                 =  slider_entropy.value;
+        distort.entropyOscillation      =  slider_entropyOscA.value;
+        distort.entropyOscillationSpeed =  slider_entropyOscS.value;
+        distort.fisheye_x               = -slider_fisheye.value;
+        distort.fisheye_y               = -slider_fisheye.value;
+        character.position              = new Vector3(slider_characterPosition.value, 0, -1);
+    }
+}
